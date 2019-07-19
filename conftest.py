edfd5ce7e8a1db962a54f71fa0fe3f8568bc3592
Alexey <aleshin@restream.rt.ru>
2019-07-19 14:56:19 +0400

    change data for run with 1 language

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -7,23 +8,18 @@ options.add_experimental_option('prefs', {'intl.accept_languages': 'user_languag
 browser = webdriver.Chrome(options=options)
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default='chrome',
-                     help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default='user_language',
-                     help="Choose language: ru or en-gb")
+    parser.addoption('--browser_name', action='store', default='chrome')
+    parser.addoption('--language', action='store', default='ru')
 
 @pytest.fixture(scope="function")
 def browser(request):
     user_language = request.config.getoption("language")
     browser_name = request.config.getoption("browser_name")
     if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
         browser = webdriver.Chrome()
     elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
         browser = webdriver.Firefox()
     else:
         print("Browser {} still is not implemented".format(browser_name))
     yield browser
-    print("\nquit browser..")
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,5 +1,9 @@
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+import time
+import pytest
 
-def test_show_basken_button(browser):
+@pytest.mark.parametrize('language', ["es", "fr", "ru"], ids=lambda val: 'language_' + val)
+def test_show_button(browser, language):
+    link = "http://selenium1py.pythonanywhere.com/{}/catalogue/coders-at-work_207/".format(language)
     browser.get(link)
+    time.sleep(10)
     browser.find_element_by_css_selector(".btn.btn-lg.btn-primary")
