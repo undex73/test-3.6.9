6a7c7ad3301694046dbcdf3c2a45a12861f8c565
Alexey <aleshin@restream.rt.ru>
2019-07-19 15:35:01 +0400

    finish change before review

--- conftest.py
+++ conftest.py
@@ -1,25 +1,15 @@
 import pytest
-import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-options = Options()
-options.add_experimental_option('prefs', {'intl.accept_languages': 'user_language'})
-browser = webdriver.Chrome(options=options)
-
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default='chrome')
     parser.addoption('--language', action='store', default='ru')
 
 @pytest.fixture(scope="function")
 def browser(request):
-    user_language = request.config.getoption("language")
-    browser_name = request.config.getoption("browser_name")
-    if browser_name == "chrome":
-        browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        browser = webdriver.Firefox()
-    else:
-        print("Browser {} still is not implemented".format(browser_name))
+    user_language = request.config.getoption('language')
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
     yield browser
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,9 +1,8 @@
 import time
-import pytest
 
-@pytest.mark.parametrize('language', ["es", "fr", "ru"], ids=lambda val: 'language_' + val)
-def test_show_button(browser, language):
-    link = "http://selenium1py.pythonanywhere.com/{}/catalogue/coders-at-work_207/".format(language)
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_show_button(browser):
     browser.get(link)
     time.sleep(10)
     browser.find_element_by_css_selector(".btn.btn-lg.btn-primary")
