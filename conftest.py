fd35cc48cb600bff5c3e5723195ae0405a181710
Alexey <aleshin@restream.rt.ru>
2019-07-22 21:30:13 +0400

    add 2 browser for testing

--- conftest.py
+++ conftest.py
@@ -1,15 +1,24 @@
-import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import pytest
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default='ru')
+    parser.addoption('--browser_name', action='store', default="chrome", help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="en", help="Select language. English is selected by default")
 
 @pytest.fixture(scope="function")
 def browser(request):
-    user_language = request.config.getoption('language')
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(options=options)
+    browser_name = request.config.getoption("browser_name")
+    language = request.config.getoption("language")
+    if browser_name == "chrome":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        print("Browser {} still is not implemented".format(browser_name))
     yield browser
     browser.quit()
